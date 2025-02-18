1b62fddf41bd1bac25ae09f72384f6e700baf995
ZAROV667 <leodeclercq2608@gmail.com>
2025-02-18 04:30:22 +0100

    Update strategy.py
    Signed-off-by: ZAROV667 <leodeclercq2608@gmail.com>

--- scripts/bot/strategy.py
+++ scripts/bot/strategy.py
@@ -4,8 +4,8 @@ def check_signal(df, execute_trade):
     recent_data =  df['TEMA20'].iloc[-11:]
     recent_min = min(recent_data)
     # Récupérer le prix actuel (par exemple, le prix de cloture)
-    if   last_row['TEMA20'] >= recent_min * 1.0000001:
-                    if last_row['slope'] is not None and last_row['slope'] > 0.06 and last_row['TEMA20'] < (last_row['TEMA50'] - 0.8):# 0.13 FOR SLOPE 
+    if   last_row['TEMA20'] >= recent_min * 1.00075:
+                    if last_row['slope'] is not None and last_row['slope'] > 0.16 and last_row['TEMA20'] < (last_row['TEMA50'] - 0.8):
                         execute_trade("BUY", last_row)
                         
                         
